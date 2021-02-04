4011fe5e797730d2f9ff733622f9ddcce561bc31
Jamila <jaja_mimi@hotmail.com>
2021-02-04 14:29:23 +0100

    fonction add task

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,52 @@
-let champ = document.querySelector('')
-let btnAdd = document.querySelector('.add')
-let mesTaches = document.querySelector('liste')
-console.log(btnAdd);
+
+import {champ, btnAdd, mesTaches, clearAll, finish, aFaire, tout} from "./variable.js"
+
+// event pour rajouter une tache
+btnAdd.addEventListener('click', ()=>{
+    console.log(mesTaches);
+    let barreTache = document.createElement('div')
+    let tache = document.createElement('span')
+    tache.innerText = champ.value;
+    tache.setAttribute('class', 'ml-2')
+    mesTaches.appendChild(barreTache)
+    let input = document.createElement('input')
+    input.style.height = "40px"
+    input.style.width = "150px"
+    input.style.display ="none"
+    input.style.marginLeft ="30px"
+    // btn valide
+    let btnValide = document.createElement('button')
+    btnValide.setAttribute('class', "bouton btn btn-primary ")
+
+    let iconeValide = document.createElement('i')
+    iconeValide.setAttribute('class', "fas fa-check-circle")
+    btnValide.append(iconeValide)   
+
+
+    // btn modif
+    let btnModif = document.createElement('button')
+    btnModif.setAttribute('class', " bouton btn btn-success ")
+
+    let iconeModif = document.createElement('i')
+    iconeModif.setAttribute('class', "fas fa-save")
+    btnModif.append(iconeModif)
+
+
+
+
+    // btn supp
+    let btnSupp = document.createElement('button')
+    btnSupp.setAttribute('class', "bouton btn btn-danger ")
+
+    let iconeSupp = document.createElement('i')
+    iconeSupp.setAttribute('class', "fas fa-trash-alt")
+    btnSupp.append(iconeSupp)
+
+
+
+
+    barreTache.append(tache,input,btnValide,btnModif,btnSupp)
+
+    // effacer champ apres le add
+    champ.value = ""
+})
---
+++ public/js/variable.js
@@ -0,0 +1,11 @@
+let champ = document.querySelector('input')
+let btnAdd = document.querySelector('.add')
+let mesTaches = document.querySelector('.liste')
+let clearAll = document.querySelector('.clear')
+
+//boutons bleus
+let finish = document.querySelector('#finish')
+let aFaire = document.querySelector('#faire')
+let tout = document.querySelector('#tout')
+
+export {champ, btnAdd, mesTaches, clearAll, finish, aFaire, tout}
